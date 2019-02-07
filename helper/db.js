54887428faa92746cc519f42424a85a410289f4b
Resul Gok <rgpro@Resul-MacBook-Pro.local>
2019-02-07 12:32:30 +0300

    unit test mocha chai

--- helper/db.js
+++ helper/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    mongoose.connect('mongodb://movieapi:159753mlab@ds223015.mlab.com:23015/movieapi', {useNewUrlParser: true});
+    mongoose.connect('mongodb://movieapi:159753mlab@ds223015.mlab.com:23015/movieapi', {useNewUrlParser: true, useCreateIndex:true });
     mongoose.connection.on('open', () => {
         console.log('MongoDB: Bağlandı.');
     });
